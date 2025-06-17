85eab3cdea75062bc06ca3ae34ff79a539adc709
Tanjina-kumkum <tanjinaakter882@gmail.com>
2025-06-17 22:18:59 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,58 @@
+var resultMark= 99;
+if (resultMark<=90) {
+    console.log('A');
+    
+    
+} 
+else if(resultMark==90 && resultMark>= 90) {
+    console.log("A+");
+    
+}
+else if (resultMark>=98 && resultMark==99)
+{ console.log("A+");
+}
+
+
+else { console.log('A-');
+    
+    
+}
+
+var resultMark1= 85 - 89;
+
+if (resultMark1>=85) {
+    console.log("A");
+    
+    
+}
+else if(resultMark1== 85 - 89) {
+    console.log("A");
+    
+}
+else {
+    console.log("A-");
+    
+    
+}
+ var gradeMark= 5;
+ if (gradeMark<5) {
+    console.log("B");
+    
+    
+ } 
+ else if(gradeMark>5 && gradeMark>=4.5){
+    console.log("B+");
+    
+ }
+ else if (gradeMark>=4 && gradeMark===5) {
+    console.log("C+");
+    
+ }
+ 
+ 
+ else { console.log("C");
+ 
+
+    
+ }
+
